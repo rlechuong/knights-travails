da52e7e2dedb904db79aaaeb5833d73dffbb1753
Richard Le <169632088+rlechuong@users.noreply.github.com>
2025-08-11 04:09:58 -0400

    Create first iteration of queue and while loop

--- src/index.js
+++ src/index.js
@@ -48,8 +48,36 @@ function getLegalMoves(square) {
   return legalMoves;
 }
 
-console.log(getLegalMoves([0, 0]));
-console.log(getLegalMoves([7, 7]));
-console.log(getLegalMoves([3, 3]));
-console.log(getLegalMoves([0, 3]));
-console.log(getLegalMoves([3, 0]));
+function knightMoves(startSquare, targetSquare) {
+  let queue = [];
+  queue.push({ parent: null, position: startSquare });
+
+  while (
+    queue.length > 0 &&
+    (queue[0].position[0] !== targetSquare[0] ||
+      queue[0].position[1] !== targetSquare[1])
+  ) {
+    let current = queue[0];
+    let legalMoves = getLegalMoves(current.position);
+    console.log(queue);
+    for (const move of legalMoves) {
+      queue.push({ parent: current, position: move });
+    }
+
+    queue.shift();
+  }
+
+  console.log(targetSquare[0]);
+  console.log(targetSquare[1]);
+  console.log(queue[0].position[0]);
+  console.log(queue[0].position[1]);
+  console.dir(queue[0]);
+}
+
+console.log(knightMoves([0, 0], [7, 7]));
+
+// console.log(getLegalMoves([0, 0]));
+// console.log(getLegalMoves([7, 7]));
+// console.log(getLegalMoves([3, 3]));
+// console.log(getLegalMoves([0, 3]));
+// console.log(getLegalMoves([3, 0]));
